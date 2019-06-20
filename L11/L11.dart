3b77210a7d3dd5059d71e42c79158ff8fd605279
Phillip <phillip7.et@gmail.com>
2019-06-20 22:13:28 +0700

    createAvenger is orderly without async await because of recursive method

--- L11/L11.dart
+++ L11/L11.dart
@@ -1,5 +1,6 @@
 import 'Avenger.dart';
 import 'Thor.dart';
+
 const List<String> urlAvengers = [
   'https://blogspotscraping.herokuapp.com/avengers/Thor.json',
   'https://blogspotscraping.herokuapp.com/avengers/Thanos.json',
@@ -7,20 +8,53 @@ const List<String> urlAvengers = [
 ];
 
 void fetchAvengers() {
-  for(final url in urlAvengers){
-    Avengers.fetchAvenger(url).then((onValue) => print(onValue["Avenger"]["name"]));
+  for (final url in urlAvengers) {
+    Avengers.fetchAvenger(url)
+        .then((onValue) => print(onValue["Avenger"]["name"]));
   }
 }
+
 Future<dynamic> createAvengers() async {
-  for(final url in urlAvengers){
-     var jsonAvenger = await Avengers.fetchAvenger(url);
-     jsonAvenger = jsonAvenger["Avenger"];
-     String name = jsonAvenger["name"];
-     String sexual = jsonAvenger["sexual"];
-     String type = Avengers.getFileNameFromURL(url);
-     Avenger avenger = Avenger(type: type, name: name, sexual: sexual);
-     avenger.showInfo();
+  for (final url in urlAvengers) {
+    var jsonAvenger = await Avengers.fetchAvenger(url);
+    jsonAvenger = jsonAvenger["Avenger"];
+    String name = jsonAvenger["name"];
+    String sexual = jsonAvenger["sexual"];
+    String type = Avengers.getFileNameFromURL(url);
+    Avenger avenger = Avenger(type: type, name: name, sexual: sexual);
+    avenger.showInfo();
   }
+}
+
+void createAvengers2() {
+  for (final url in urlAvengers) {
+    Avengers.fetchAvenger(url).then((jsonAvenger) {
+      jsonAvenger = jsonAvenger["Avenger"];
+      String name = jsonAvenger["name"];
+      String sexual = jsonAvenger["sexual"];
+      String type = Avengers.getFileNameFromURL(url);
+      Avenger avenger = Avenger(type: type, name: name, sexual: sexual);
+      avenger.showInfo();
+    });
+  }
+}
+
+void createAvengersRecursive(int i) {
+  String url = urlAvengers[i];
+  Avengers.fetchAvenger(url).then((jsonAvenger) {
+    jsonAvenger = jsonAvenger["Avenger"];
+    String name = jsonAvenger["name"];
+    String sexual = jsonAvenger["sexual"];
+    String type = Avengers.getFileNameFromURL(url);
+    Avenger avenger = Avenger(type: type, name: name, sexual: sexual);
+    avenger.showInfo();
+    i++;
+    if (i < urlAvengers.length) {
+      createAvengersRecursive(i);
+    } else {
+      print('Done');
+    }
+  });
 }
 
 void main() async {
@@ -28,7 +62,7 @@ void main() async {
   //fetchAvengers();
   //print(Avengers.getFileNameFromURL(urlAvengers[0]));
 
-  // =========== Method 1( use Future then 1st) =========== 
+  // =========== Method 1( use Future then 1st) ===========
   // print('============ Named factory constructor ============');
   // Avenger.fromURL(urlAvengers[2]);
   // Future.delayed(Duration(seconds: 3), () => {
@@ -36,9 +70,13 @@ void main() async {
   //   createAvengers()
   // });
 
-  // =========== Method 2( use Future async await 1st) =========== 
-  print('============ L11.2 - Call Default Factory Constructor Orderly ============');
-  await createAvengers();
-  print('============ L11.1 - Named factory constructor ============');
-  Avenger.fromURL(urlAvengers[2]);
+  // =========== Method 2( use Future async await 1st) ===========
+  print(
+      '============ L11.2 - Call Default Factory Constructor Orderly ============');
+  //await createAvengers();
+  createAvengers2();
+  // print('============ L11.1 - Named factory constructor ============');
+  // Avenger.fromURL(urlAvengers[2]);
+  //print('============ L11.3 - Recursive ============');
+  //createAvengersRecursive(0);
 }
